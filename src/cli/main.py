667bd2d0dcafc599c5de580a71436d5fcec04596
iBrotNano <marcelmelzig@gmail.com>
2026-02-10 12:07:05 +0100

    #1 Shows the main menu in a endless loop

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,37 +1,41 @@
-from address.address import Address
-from project.project import Project
-from project.merkle_root import MerkleRoot
-from project.command_line import CommandLine
+import sys
+
+from config import config
 from config.console import console
 from address.custom_address import CustomAddress as CustomAddress
 from address.solana_address import SolanaAddress as SolanaAddress
+from main_menu import CommandLine as menu
+from address.command_line import CommandLine as address_cli
 
-custom_address = CustomAddress("Custom", Address.generate_mnemonic())
-console.print(f"Generated custom address: {custom_address.get_address()}")
-console.print(f"Public key (raw, hex): {custom_address.get_pubkey()}")
-console.print(f"Mnemonic: {custom_address.get_mnemonic()}")
-console.print(custom_address.get_keypair())
-custom_address.save()
-loaded = custom_address.load(custom_address.name)
-console.print(f"Loaded custom address: {loaded.get_address()}")
-console.print(f"Public key (raw, hex): {loaded.get_pubkey()}")
-console.print(f"Mnemonic: {loaded.get_mnemonic()}")
-console.print(loaded.get_keypair())
 
-solana_address = SolanaAddress("Solana", Address.generate_mnemonic())
-console.print(f"Generated Solana address: {solana_address.get_address()}")
-console.print(f"Public key (raw, hex): {solana_address.get_pubkey()}")
-console.print(f"Mnemonic: {solana_address.get_mnemonic()}")
-console.print(solana_address.get_keypair())
-solana_address.save()
-loaded = solana_address.load(solana_address.name)
-console.print(f"Loaded Solana address: {loaded.get_address()}")
-console.print(f"Public key (raw, hex): {loaded.get_pubkey()}")
-console.print(f"Mnemonic: {loaded.get_mnemonic()}")
-console.print(loaded.get_keypair())
+def show_main_menu():
+    """
+    Displays the main menu and handles user input.
+    """
+    try:
+        command = main_menu.show()
 
-# project_cli = CommandLine()
-# project = Project(project_cli.select_project_path())
-# merkle_root = MerkleRoot(project)
-# merkle_root.compute_root()
-# project_cli.display_merkle_root(merkle_root)
+        if command == main_menu.KEY_MANAGEMENT_COMMAND:
+            address_cli().show()
+
+        if command == main_menu.EXIT_COMMAND or command is None:
+            console.print("Goodbye! 👋")
+            exit(0)
+
+    except Exception as e:
+        print(e)
+        # log.exception(f"An error of type {type(e)} occurred. Message: {e}")
+
+
+try:
+    config.configure()
+    console.print("Welcome to the Plockchain CLI!")
+    console.rule()
+    main_menu = menu()
+
+    while True:
+        show_main_menu()
+# Catch any unexpected errors at the top level during app initialization.
+except Exception as e:
+    # log.critical(f"An error of type {type(e)} occurred. Message: {e}", exc_info=True)
+    exit(1)
---
+++ src/cli/main_menu.py
@@ -0,0 +1,28 @@
+import questionary
+
+
+class CommandLine:
+    EXIT_COMMAND = "EXIT"
+    KEY_MANAGEMENT_COMMAND = "KEY_MANAGEMENT"
+
+    def show(self) -> str | None:
+        """
+        Displays the main menu and handles user input.
+
+        Returns:
+            str: The command selected by the user, or None if the user cancels.
+        """
+        choices = [
+            questionary.Choice(
+                "Key management",
+                value=self.KEY_MANAGEMENT_COMMAND,
+            ),
+            questionary.Choice(
+                "Exit the application",
+                value=self.EXIT_COMMAND,
+            ),
+        ]
+
+        return questionary.select(
+            "What do you want to do?", choices=choices, use_shortcuts=True
+        ).ask()
